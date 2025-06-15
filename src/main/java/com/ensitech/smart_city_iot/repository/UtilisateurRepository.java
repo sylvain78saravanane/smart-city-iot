abcb6d5d51814775b0d135619d00640d2251260d
Sylvain SARAVANANE <sylvainsinou78@gmail.com>
2025-06-15 16:53:36 +0200

    Utilisateur Repository

--- src/main/java/com/ensitech/smart_city_iot/repository/UtilisateurRepository.java
+++ src/main/java/com/ensitech/smart_city_iot/repository/UtilisateurRepository.java
@@ -1,13 +1,40 @@
 package com.ensitech.smart_city_iot.repository;
 
-import com.ensitech.smart_city_iot.entity.Citoyen;
-import com.ensitech.smart_city_iot.entity.Utilisateur;
+import com.ensitech.smart_city_iot.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    Utilisateur findByEmail(String email);
 
+    List<Utilisateur> findByActif(Boolean actif);
+
+    List<Utilisateur> findByNomContainingIgnoreCaseOrPrenomContainingIgnoreCase(String nom, String prenom);
+
+    @Query("SELECT u FROM Utilisateur u WHERE TYPE(u) = :type")
+    List<Utilisateur> findByType(@Param("type") Class<? extends Utilisateur> type);
+
+    boolean existsByEmail(String email);
+
+    @Query("SELECT COUNT(u) FROM Utilisateur u WHERE TYPE(u) = :type")
+    Long countByType(@Param("type") Class<? extends Utilisateur> type);
+
+    // Méthodes spécifiques par type
+    @Query("SELECT u FROM Utilisateur u WHERE TYPE(u) = Administrateur")
+    List<Administrateur> findAllAdministrateurs();
+
+    @Query("SELECT u FROM Utilisateur u WHERE TYPE(u) = GestionnaireDeVille ")
+    List<GestionnaireDeVille> findAllGestionnaires();
+
+    @Query("SELECT u FROM Utilisateur u WHERE TYPE(u) = Chercheur")
+    List<Chercheur> findAllChercheurs();
+
+    @Query("SELECT u FROM Utilisateur u WHERE TYPE(u) = Citoyen")
+    List<Citoyen> findAllCitoyens();
 }
