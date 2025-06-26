8877d1d9f62d5e20348f0d89c9186527e1cc10b2
Sylvain SARAVANANE <sylvainsinou78@gmail.com>
2025-06-27 01:40:34 +0200

    add JWT Filter in SecurityConfig

--- src/main/java/com/ensitech/smart_city_iot/config/SecurityConfig.java
+++ src/main/java/com/ensitech/smart_city_iot/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.ensitech.smart_city_iot.config;
 // TODO : Ajouter la dépendance Spring Security
 // TODO : Réaliser la configuartion de sécurité (csrf)
 
+import com.ensitech.smart_city_iot.service.jwt.JwtAuthenticatonFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +24,8 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Autowired
+    private JwtAuthenticatonFilter authenticatonFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -32,6 +37,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,"/api/v1/utilisateurs").permitAll()
                         .anyRequest().authenticated()
                 )
+        .addFilterBefore(authenticatonFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
     }
 }
